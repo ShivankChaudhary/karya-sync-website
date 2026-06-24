4b1c3ec549f9e1649ac7a6730b0d6aadc5964236
Amay Bhatnagar <98695229+Dodger1351@users.noreply.github.com>
2026-06-24 22:53:02 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // Static export for S3
+  output: "export",
+
+  // Required for static hosting (S3 does not support image optimization)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
